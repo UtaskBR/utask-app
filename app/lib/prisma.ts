c340cc1d8b1809fdc0edc164d948d2b4dd1c22a0
UtaskBR <leonardo.ferreira@utask.com.br>
2025-04-26 18:26:16 -0300

    mais mods para a integracao

--- app/lib/prisma.ts
+++ app/lib/prisma.ts
@@ -1,11 +1,12 @@
-import { PrismaClient } from '@prisma/client'
+// app/lib/prisma.ts
 
-declare global {
-  var prisma: PrismaClient | undefined
-}
+import { PrismaClient } from '@prisma/client';
 
-const prisma = global.prisma || new PrismaClient()
+// Evita múltiplas instâncias do Prisma Client em desenvolvimento
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-if (process.env.NODE_ENV !== 'production') global.prisma = prisma
+export const prisma = globalForPrisma.prisma || new PrismaClient();
 
-export default prisma
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
+
+export default prisma;
