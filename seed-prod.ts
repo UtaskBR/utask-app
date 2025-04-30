a29ddc4ae398fc6317ad055b6b2e80e5064208ab
UtaskBR <leonardo.ferreira@utask.com.br>
2025-04-30 18:16:20 -0300

    seed-prod fix

--- seed-prod.ts
+++ seed-prod.ts
@@ -1,17 +1,13 @@
-// seed-prod.ts
 import { PrismaClient } from '@prisma/client';
 import { professions } from './prisma/seed/professions';
 
 const prisma = new PrismaClient();
 
 async function main() {
-  for (const profession of professions) {
-    await prisma.profession.upsert({
-      where: { name: profession.name },
-      update: {},
-      create: { name: profession.name, icon: profession.icon },
-    });
-  }
+  await prisma.profession.createMany({
+    data: professions,
+    skipDuplicates: true,
+  });
 }
 
 main()
