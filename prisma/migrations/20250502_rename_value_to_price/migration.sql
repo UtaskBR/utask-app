405efe4a613a126e027ce785494b748e7524d85f
UtaskBR <leonardo.ferreira@utask.com.br>
2025-05-04 00:15:57 -0300

    VALUE TO PRICE NO BID FIX

--- prisma/migrations/20250502_rename_value_to_price/migration.sql
+++
@@ -1,3 +0,0 @@
--- Renomeação de value para price já aplicada diretamente no banco.
--- Esta entrada serve apenas para alinhar o histórico de migrações.
-ALTER TABLE "Service" ADD COLUMN "price" DOUBLE PRECISION;
